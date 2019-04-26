ec27a9652cebf3006d62cc1f7946117037ba9aa8
yuejn <jennifer8ho@gmail.com>
2019-04-26 16:02:33 +0200

    init Express

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express running → PORT ${server.address().port}`)
+})
