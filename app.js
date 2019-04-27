c8a305eb496fb16b67f57b0a99ee9ceccad2b340
yuejn <jennifer8ho@gmail.com>
2019-04-27 11:35:46 +0200

    App initialisation through app.js.

---
+++ app.js
@@ -0,0 +1,42 @@
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+const fs = require('fs')
+const join = require('path').join
+const express = require('express')
+const bodyParser = require('body-parser')
+const expressValidator = require('express-validator')
+const errorHandlers = require('./handlers/errorHandlers');
+const models = join(__dirname, 'models')
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(expressValidator())
+
+// load models before routes
+fs.readdirSync(models)
+  .filter(file => ~file.search(/^[^.].*\.js$/))
+  .forEach(file => {
+    require(join(models, file))
+  });
+
+const router = require('./routes')
+
+app.use('/', router)
+app.use(errorHandlers.notFound);
+app.use(errorHandlers.productionErrors);
+
+app.set('port', process.env.PORT || 3000)
+
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  dbName: 'projects_' + process.env.NODE_ENV.toLowerCase(),
+  useFindAndModify: false
+})
+mongoose.Promise = global.Promise
+mongoose.set('useCreateIndex', true)
+const db = mongoose.connection
+db.on('error', err => console.error(err.message))
+
+module.exports = app
